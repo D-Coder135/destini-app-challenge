63f1f878a741b4a19db31de0174064815ea453f0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:37:40 +0530

    add a statement inside the body of the else block

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -52,7 +52,9 @@ class StoryBrain {
     } else if (storyNumber == 2) {
       if (choiceNumber == 1) {
         storyNumber = 5;
-      } else {}
+      } else {
+        storyNumber = 4;
+      }
     }
   }
 }
