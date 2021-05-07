fd0c6a84f3c8fb3b8938eb2d1cf256b45a37922f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:58:18 +0530

    Remove the TODO containing the step 27

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -78,5 +78,3 @@ class StoryBrain {
     }
   }
 }
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
