639481cf5671a6d49bf318b4226858cf96361938
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:00:33 +0530

    Add a return statement inside the body of the method getChoice2()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -36,7 +36,9 @@ class StoryBrain {
     return _storyData[0].firstChoice;
   }
 
-  String getChoice2() {}
+  String getChoice2() {
+    return _storyData[0].secondChoice;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
