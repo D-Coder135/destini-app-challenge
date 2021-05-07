f9de9bc9ad062e3c04fe7c7ff2be5bd2d995e58b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:44:23 +0530

    Remove the TODO containing the step 22

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -75,6 +75,4 @@ class StoryBrain {
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
