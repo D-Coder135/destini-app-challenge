451ab034f2d603e33ddb0c7e9968e29e543e0b36
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:33:16 +0530

    add a if() block inside the body of the method nextStory()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -42,7 +42,9 @@ class StoryBrain {
     return _storyData[0].secondChoice;
   }
 
-  void nextStory(int choiceNumber) {}
+  void nextStory(int choiceNumber) {
+    if (storyNumber == 0) {}
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
