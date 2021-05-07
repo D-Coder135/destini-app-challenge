aa3dc1a7b8bea09241c360b3b0ea92ef9bad1763
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:43:11 +0530

    add a method restart() of type void

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -65,6 +65,8 @@ class StoryBrain {
       restart();
     }
   }
+
+  void restart() {}
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
