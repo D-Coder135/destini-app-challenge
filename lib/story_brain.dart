cc70dc5d465e84cd5b694054427e084936b3b5da
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:46:00 +0530

    Remove the TODO containing the step 23

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -71,8 +71,6 @@ class StoryBrain {
   }
 }
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
