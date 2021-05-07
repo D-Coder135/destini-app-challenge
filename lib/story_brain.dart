37392017e2ee6d1a1030576facc19c6fb647f074
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:25:43 +0530

    add a field of type int inside the file story_brain.dart and initialize it with value 0

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,8 @@
 import 'story.dart';
 
 class StoryBrain {
+  int storyNumber = 0;
+
   List<Story> _storyData = [
     Story(
         'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
