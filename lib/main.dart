042df14dbd431397a0aa6eeac4edde9c1aeca035
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 17:00:03 +0530

    set the visible property of the Visibility() widget equal to the output of the buttonShouldBeVisible() method

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
                   // ignore: deprecated_member_use
                   child: FlatButton(
                     onPressed: () {
