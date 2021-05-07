6082e75e21f3f0ed10e084f6c69bbaf968bd7353
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 15:55:59 +0530

    Remove the TODO's containing steps 5,6,7 and 8

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -37,15 +37,7 @@ class StoryBrain {
   }
 }
 
-//TODO: Step 6 - import the story.dart file into this file.
-
-//TODO: Step 5 - Create a new class called StoryBrain.
-
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
-
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
