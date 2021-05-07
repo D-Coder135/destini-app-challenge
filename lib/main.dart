0bc9dcd693b9a2c0036e90dd3bb28ce743325ffb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:03:42 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _StoryPageState extends State<StoryPage> {
                   child: Text(
                     //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                     storyBrain.getChoice2(),
-                    'Choice 2',
+                    // 'Choice 2',
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
