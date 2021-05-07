0c9db04bb71ae269c3904ba6243b09a0c91f01b3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:27:18 +0530

    add a method of type void name nextStory() inside the class StoryBrain

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -41,6 +41,8 @@ class StoryBrain {
   String getChoice2() {
     return _storyData[0].secondChoice;
   }
+
+  void nextStory(int userChoiceNumber) {}
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
