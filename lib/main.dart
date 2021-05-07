8ecc84ea786abe986e7d685c17298502ddfd051f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 14:57:45 +0530

    remove the TODO containing Step 1 from the code

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
