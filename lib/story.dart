6ba985c17bb4491bd9f1166a2af3a9506951df10
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 15:15:47 +0530

    Remove TODO's containing steps 3 and 4

--- lib/story.dart
+++ lib/story.dart
@@ -5,6 +5,3 @@ class Story {
 
   Story(this.storyTitle, this.firstChoice, this.secondChoice);
 }
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
-
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
