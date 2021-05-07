640895003d3a0561196e36d8f76907c4c08f9b78
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:27:47 +0530

    Add some fix up

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -42,7 +42,7 @@ class StoryBrain {
     return _storyData[0].secondChoice;
   }
 
-  void nextStory(int userChoiceNumber) {}
+  void nextStory(int choiceNumber) {}
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
