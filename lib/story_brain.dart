60c3aa7f6cd0d2a35c8de75b1f20aa7d45468533
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:43:53 +0530

    initialize the variable storyNumber with value 0 inside the body of the method restart()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -66,7 +66,9 @@ class StoryBrain {
     }
   }
 
-  void restart() {}
+  void restart() {
+    storyNumber = 0;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
