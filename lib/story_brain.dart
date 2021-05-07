d4fded477cd157c3f8454dd6f17efa866b947fbc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:56:35 +0530

    add a if() block along with a condition inside the body of the method buttonShouldBeVisible()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -70,7 +70,9 @@ class StoryBrain {
     _storyNumber = 0;
   }
 
-  bool buttonShouldBeVisible() {}
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {}
+  }
 }
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
