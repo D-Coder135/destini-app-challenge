be8ae3a02e0be8ac079c009d9875397efd74361d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 15:58:11 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,9 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    storyBrain.getStory();
+                    storyBrain.getStory(),
                     //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    // 'Story text will go here.',
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
