20bcdf19ecf3d0e065b6a5fd4dbe1cf03d054794
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-07 16:24:27 +0530

    Remove the TODO containing step 15

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'story_brain.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
